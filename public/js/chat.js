c9b44de8692fcfdc9785c13702de3bd2379d76c2
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-11-17 16:27:31 +0000

    Restructure folders and setup mustache templating

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,43 @@
 const socket = io();
 
-socket.on('message', msg => console.log(msg));
-
 const form = document.querySelector('#chat-form');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+const textarea = document.querySelector('#chat-form textarea');
+const submitBtn = document.querySelector('#chat-form button');
+const shareBtn = document.querySelector('#share-location');
+const messages = document.querySelector('#messages');
+const messageTemplate = document.querySelector('#message-template').innerHTML;
 
-    const message = this.elements.message.value;
-
-    socket.emit('message', message);
+socket.on('message', message => {
+    const html = Mustache.render(messageTemplate, {
+        message
+    });
+    messages.insertAdjacentHTML('beforeend', html);
 });
 
-document.querySelector('#share-location').addEventListener('click', () => {
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    submitBtn.setAttribute('disabled', 'disabled');
+    const message = this.elements.message.value;
+
+    socket.emit('message', message, (error) => {
+        submitBtn.removeAttribute('disabled');
+        textarea.value = '';
+        textarea.focus();
+        if (error) return console.log(error);
+
+        console.log('the message was delievered', m);
+    });
+});
+
+shareBtn.addEventListener('click', () => {
     if (!navigator.geolocation) return alert('Geolocation is not supported by your browser.');
+    shareBtn.setAttribute('disabled', 'disabled');
 
     navigator.geolocation.getCurrentPosition(pos => {
         const { latitude, longitude } = pos.coords;
-        socket.emit('location', { latitude, longitude });
+        socket.emit('location', { latitude, longitude }, () => {
+            shareBtn.removeAttribute('disabled');
+            console.log('Location shared successfully');
+        });
     }, e => console.error(e));
 });
--- src/server.js
+++ src/server.js
@@ -1,40 +1,33 @@
 const path = require("path");
 const http = require('http');
 const express = require("express");
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
 const port = process.env.PORT;
-const hbs = require("hbs");
 
 const publicFolder = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
-const partialsPath = path.join(__dirname, '../templates/partials');
-
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
-
-hbs.registerPartials(partialsPath);
 app.use(express.static(publicFolder));
-
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Socket-To-Me'
-    });
-});
 
 io.on('connection', socket => {
     socket.emit('message', 'Welcome!');
     socket.broadcast.emit('message', 'A new user has joined!');
 
-    socket.on('message', msg => {
+    socket.on('message', (msg, cb) => {
+        const filter = new Filter();
+
+        if (filter.isProfane(msg)) return cb('Profanity is not allowed');
+
         io.emit('message', msg);
+        cb('delievered');
     });
 
-    socket.on('location', loc => {
+    socket.on('location', (loc, cb) => {
         io.emit('message', `Shared location: https://google.com/maps?q=${loc.latitude},${loc.longitude}`);
+        cb();
     });
 
     socket.on('disconnect', () => {
